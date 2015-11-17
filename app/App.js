69a778614e087759b6cc372a3e948ea874dcad3d
eanplatter <eanplatter@gmail.com>
2015-11-17 16:17:11 -0600

    spacing update

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { render }           from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 
 class App extends Component {
   render() {
