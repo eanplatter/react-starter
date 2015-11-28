5f3f7e100d0e65454324ceba4817cd693eaae3f8
Andrew Bethel <andrew@striking.ly>
2015-11-27 19:11:17 -0600

    update to babel v6

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var HTMLWebpackPlugin = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  hash: true,
+  filename: 'index.html',
+  inject: 'body'
+});
+var HotReloader = new webpack.HotModuleReplacementPlugin();
+
 module.exports = {
-  entry: './app/App.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app/App.js'
+  ],
   output: {
     path: 'dist',
     filename: 'index_bundle.js',
@@ -10,18 +23,10 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel',
-        query: {
-          stage: 0
-        },
+        loader: 'react-hot!babel',
         include: __dirname + '/app'
       },
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: __dirname + '/app/index.html',
-    hash: true,
-    filename: 'index.html',
-    inject: 'body'
-  })]
+  plugins: [HTMLWebpackPlugin, HotReloader]
 };
